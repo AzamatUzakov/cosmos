151903103ba009227b47825736a3358da199b2ea
AzamatUzakov <azamatakmal053@gmail.com>
2024-05-19 02:53:15 +0500

    fix

--- src/js/azamat.js
+++ src/js/azamat.js
@@ -1,8 +1,26 @@
 
-
+/* 
 let sputnik = document.querySelector('.sputnik-position')
 
 sputnik.onclick = () => {
 sputnik.classList.toggle('animatetSputnik')
-}
+} */
 
+
+let pages = document.querySelectorAll('.first-SPA-box, .second-SPA-box,.third-SPA-box, .fouth-SPA-box, .fifth-SPA-box, .sixth-SPA-box')
+let pagesBtn = document.querySelectorAll("[data-page]")
+
+pagesBtn.forEach(btn => {
+    btn.onclick = () => {
+        let id = btn.getAttribute('data-page')
+
+        pages.forEach(page => {
+            if (id === page.id) {
+                page.classList.add('show-page')
+            }else{
+                page.classList.remove('show-page')
+            }
+        });
+    }
+})
+console.log(pages);
